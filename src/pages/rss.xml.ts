f69898191d571c8159733d9b6a205ca04bbe5822
Kyota Sakagami <kyouta02102005@gmail.com>
2026-03-15 15:55:07 +0900

    correct right ogp image

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,7 +16,7 @@ export async function GET(context: APIContext) {
             title: post.data.title,
             description: post.data.description,
             pubDate: post.data.pubDate,
-            link: `/posts/${post.id}/`,
+            link: `/posts/${post.slug}/`,
         })),
     });
 }
